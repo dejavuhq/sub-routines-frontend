639a7aef52158537640edf51c6aeed003d32c174
Alex <ajf@fastmail.com>
2020-06-30 19:10:29 -0500

    hello world

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <header className='header'>
+      <h1>Hello World</h1>
+    </header>
+  );
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import Home from './components/Home';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Home />, document.getElementById('app'));
