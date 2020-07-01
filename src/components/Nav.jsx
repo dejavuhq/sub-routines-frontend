cfceefab6c2fca954dd5f4cb8d92f3c98415ab0c
Alex <ajf@fastmail.com>
2020-07-01 12:08:36 -0500

    ADD navigation

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Buscar</li>
+        <li>Perfil</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
