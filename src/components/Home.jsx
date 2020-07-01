0966fa6530c2c01be43e0fc9af33dd3d2e559a29
Alex <ajf@fastmail.com>
2020-07-01 12:00:55 -0500

    Basic Home structure

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = () => {
   return (
     <header className='header'>
-      <h1>Hello World</h1>
+      <p>Miércoles 1 de julio de 2020</p>
+      <h2>Hábitos para hoy:</h2>
+      <p>Aún no has registrado ningún hábito <button>Agregar Hábito</button></p>
     </header>
   );
 }
