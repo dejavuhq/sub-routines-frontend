66e4aa88b82e7cb08da82998f5e06550f29a7b62
Alex <ajf@fastmail.com>
2020-07-01 11:01:54 -0500

    FIX routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // import Home from '../components/Home';
-// import Login from '../components/Login';
+import Login from '../components/Login';
 import { Register } from '../Pages/Register/index';
-// import NotFound from '../components/NotFound';
+import NotFound from '../components/NotFound';
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../assets/styles/main.scss";
 
 const App = () => (
   <BrowserRouter>
       <Switch>
         <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+        <Route component={NotFound} />
       </Switch>
   </BrowserRouter>
 );
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <header className='header'>
+      <h1>404 Not Found</h1>
+    </header>
+  );
+}
+
+export default Home;
