4165277b685bf41b6d7babd702b5573d445964ea
David Perez <dperezval08@gmail.com>
2020-07-01 17:12:57 -0500

    [ADD] Built Register layout

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -3,41 +3,35 @@ import "../assets/styles/components/RegisterForm.scss";
 
 export const RegisterForm = () => {
   return (
-    <main>
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h1 className="text-center">Registra tus hábitos diarios</h1>
-            <form>
-              <div className="form-group">
-                <label>Nombres</label>
-                <input className="form-control" type="text"/>
-              </div>
-              <div className="form-group">
-                <label>Apellidos</label>
-                <input className="form-control" type="text"/>
-              </div>
-              <div className="form-group">
-                <label>Correo electronico</label>
-                <input className="form-control" type="email"/>
-              </div>
-              <div className="form-group">
-                <label>Username</label>
-                <input className="form-control" type="text"/>
-              </div>
-              <div className="form-group">
-                <label>Nueva contraseña</label>
-                <input className="form-control" type="password"/>
-              </div>
-              <div className="form-group">
-                <label>Confirma la contraseña</label>
-                <input className="form-control" type="password"/>
-              </div>
-              <button type="submit" className="btn btn-primary">Continuar</button>
-            </form>
-          </div>
+    <main className="main">
+      <h1 className="title">Regístrate</h1>
+      <form className="form">
+        <div className="form-group">
+          <label className="form-group__label">Nombres</label>
+          <input className="form-control" type="text"/>
         </div>
-      </div>
+        <div className="form-group">
+          <label className="form-group__label">Apellidos</label>
+          <input className="form-control" type="text"/>
+        </div>
+        <div className="form-group">
+          <label className="form-group__label">Correo electrónico</label>
+          <input className="form-control" type="email" placeholder="email@ejemplo.com"/>
+        </div>
+        <div className="form-group">
+          <label className="form-group__label">Username</label>
+          <input className="form-control" type="text"/>
+        </div>
+        <div className="form-group">
+          <label className="form-group__label">Nueva contraseña</label>
+          <input className="form-control" type="password"/>
+        </div>
+        <div className="form-group">
+          <label className="form-group__label">Confirma la contraseña</label>
+          <input className="form-control" type="password"/>
+        </div>
+        <button type="submit" className="btn">Continuar</button>
+      </form>
     </main>
   )
 };
