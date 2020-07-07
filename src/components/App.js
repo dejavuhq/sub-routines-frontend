d3e42ccdce3d72c230319dcd54dc0dba60a76f1e
Alex <ajf@fastmail.com>
2020-07-07 13:24:43 -0500

    [UPDATE] Home as a private route

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
 import Home from '../components/Home';
 import Login from '../components/Login';
 import { Register } from '../Pages/Register';
 import NotFound from '../components/NotFound';
 import { RegisterValidation } from "../Pages/RegisterValidation";
+import { AuthContext } from '../context/auth';
 import "../assets/styles/main.scss";
 
-const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path='/home' component={Home} />
-      <Route exact path='/register' component={Register} />
-      <Route exact path='/login' component={Login} />
-      <Route exact path='/register/validation' component={RegisterValidation} />
-      <Route component={NotFound} />
-    </Switch>
-  </BrowserRouter>
+const App = (props) => (
+  <AuthContext.Provider value={false}>
+    <BrowserRouter>
+      <Switch>
+        <PrivateRoute exact path='/home' component={Home} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register/validation' component={RegisterValidation} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  </AuthContext.Provider>
 );
 
 export default App;
