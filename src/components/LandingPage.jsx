c9dbd3424919f1515bbb1c5c183e046616ecd254
Alex <ajf@fastmail.com>
2020-07-12 12:45:43 -0500

    [ADD] import images in landing page component

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import logo from '../assets/static/logo.png';
+import heatmap from '../assets/static/heatmap.png';
 import '../assets/styles/components/LandingPage.scss';
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
       <header className='header'>
-        <img src="../assets/static/logo.png" alt="dejavu logo" />
+        <img src={logo} alt="dejavu logo" />
       </header>
       <h1>Track your Habits <br /> Reach your goals</h1>
       <button className="cta-btn"><Link to="/register">Get Started</Link></button>
-      <img src={require('../assets/static/heatmap.png')} alt="heatmap" />
+      <img src={heatmap} alt="heatmap" />
     </div>
   );
 }
