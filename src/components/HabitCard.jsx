3a7f7df57f95887f213c186b48724fab2a5562ce
David Perez <dperezval08@gmail.com>
2020-07-12 21:51:52 -0500

    [ADD] stats in Profile

--- src/components/HabitCard.jsx
+++ src/components/HabitCard.jsx
@@ -6,11 +6,11 @@ export const HabitCard = (props) => {
     <div className="habitCard">
       <h3 className="habitCard__name">{props.data.name}</h3>
       <p className="habitCard__description">{props.data.description}</p>
-      <p className="habitCard__frequency">Frecuencia: {props.data.recurrence}</p>
-      <p className="habitCard__startDate">Inicio: {props.data.start_date}</p>
+      <p className="habitCard__frequency">Frequency: {props.data.recurrence}</p>
+      <p className="habitCard__startDate">Start: {props.data.start_date}</p>
       {
         props.data.end_date
-        ? <p className="habitCard__finishDate">Final: {props.data.end_date}</p>
+        ? <p className="habitCard__finishDate">End: {props.data.end_date}</p>
         : null
       }
     </div>
--- src/components/UserStats.jsx
+++ src/components/UserStats.jsx
@@ -1,14 +1,31 @@
-import React from "react";
-import { HabitCard } from "./HabitCard";
+import React, { useState, useEffect, useContext} from "react";
+import UserContext from '../context/UserContext';
 import "../assets/styles/components/Stats.scss";
 
 export const UserStats = () => {
+  const { user, setUser } = useContext(UserContext);
+  const [data, setData] = useState({})
+  useEffect(() => {
+    let requestOptions = {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${user.token}`,
+      }
+    }
+    fetch("https://dejavuhq.xyz/api/stats", requestOptions)
+      .then(response => response.json())
+      .then(result => setData(result[0]))
+      .catch(error => console.log('error', error));
+  }, [])
+
   return (
     <section className="stats">
-      <p>Aqui van las estadísticas del usuario</p>
-      <HabitCard />
-      <HabitCard />
-      <HabitCard />
+      <h3 className="stats__title">Current date: {data.date}</h3>
+      <p className="stats__stat">Completion rate: {data.completion_rate*100}%</p>
+      <p className="stats__stat">Completion streak: {data.completion_streak}</p>
+      <p className="stats__stat">Total habits: {data.total_habits}</p>
+      <p className="stats__stat">Habits for today: {data.total_habits_today}</p>
+      <p className="stats__stat">Done habits: {data.total_habits_done_today}</p>
     </section>
   )
 }
